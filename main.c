5e2339a584a88a92192d1a413650be2ab39ded22
wooJake <167344799+wooJake@users.noreply.github.com>
2024-10-19 13:09:50 -0700

    Update main.c

--- main.c
+++ main.c
@@ -28,30 +28,40 @@ int main(){
     int width = 167;
     int height = 100;
     
+    //Setting bounds of map.
     double xLeft = -2.0;
     double xRight = 0.47;
     double yDown = -1.12;
     double yUp = 1.12;
     
+    //Making variables to ensure set is fully visible.
     double dx = (xRight - xLeft) / (width - 1);
     double dy = (yUp - yDown) / (height - 1);
     
+    //Parallelizing main loop.
     #pragma omp parallel for
     for(int i = 0; i < height; ++i){
         for(int j = 0; j < width; ++j){
             
+            //Starting from top left of map to scaled coordinate.
+            //j is positive because we are counting up from the left of the map.
+            //i is negative because we are counting down from the top of the map.
             double x = xLeft + j * dx;
             double y = yUp - i * dy;
             
+            //Obtaining limit reached from Mandel function.
             int val = Mandel(x,y);
 
+            //Looping through visual output.
             for(int k = 0; k < 10; ++k){
 
+                //Checking if value is under a select amount before outputting specific characters.
                 if(val <= (k / 10.0) * limit){
 
                     printf("%c", chars[k]);
                     break;
                 }
+                //If value is exactly the limit we color it with the @ symbol.
                 if(val == limit){
 
                     printf("@");
@@ -59,6 +69,7 @@ int main(){
                 }
             }
         }
+        //Going to next line.
         printf("\n");
     }
 
